93a8bc979237042fff709fcfccb001a4ad424a61
Ryan Bascom <ryanbascom@gmail.com>
2019-07-26 13:01:07 -0600

    Removed client_id and client_secret

--- MusaicFM/Constants.m
+++ MusaicFM/Constants.m
@@ -8,7 +8,7 @@
 
 #import "Constants.h"
 
-NSString *const spotifyClientId = @"22a91cde404042d9a329cdfc99b5815c";
-NSString *const spotifySecretId = @"a949d331bb594d77a255c2afd4d8d887";
+NSString *const spotifyClientId = @"";
+NSString *const spotifySecretId = @"";
 NSString *const spotifyRedirectUrl = @"musaicfm://spotify";
 NSString *const lastFmId = @"";
